fd845b322fe6059bbd54d34ab1e10a97c9857db8
masysz <67843831+masysz@users.noreply.github.com>
2024-12-27 21:54:01 +0700

    Update contracts.ts

--- utils/contracts.ts
+++ utils/contracts.ts
@@ -4,9 +4,9 @@ import { getContract } from "thirdweb";
 import { STAKING_CONTRACT_ABI } from "./stakingContractABI";
 
 // Replace <contract_address> with the contract address of your contract
-const stakeTokenContractAddress = "<contract_address>";
-const rewardTokenContractAddress = "<contract_address>";
-const stakingContractAddress = "<contract_address>";
+const stakeTokenContractAddress = "<0x00EDdD9621Fb08436d0331c149D1690909a5906d>";
+const rewardTokenContractAddress = "<0xd90f7548892F94932Caa534a271a118c3f04e228>";
+const stakingContractAddress = "<0xfABa86c326d2031D6ECFa8345F7Cd70F2eB764A3>";
 
 export const STAKE_TOKEN_CONTRACT = getContract({
     client: client,
