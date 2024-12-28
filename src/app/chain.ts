912e762af897b9bc8be5dcf0c69982a516527ee7
masysz <67843831+masysz@users.noreply.github.com>
2024-12-28 12:24:43 +0700

    Update chain.ts

--- src/app/chain.ts
+++ src/app/chain.ts
@@ -1,4 +1,4 @@
 import { defineChain } from "thirdweb";
 
 // Replace <chain_id> with the chain id of your chain
-export const chain = defineChain(parseInt(process.env.NEXT_PUBLIC_CHAIN_ID!) || 1480);
+export const chain = defineChain(1480);
